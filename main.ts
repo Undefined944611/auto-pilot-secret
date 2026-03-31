42fc1c1b85a63fb2f31929a4fa479afe7ab64dc4
Undefined944611 <Spietragallo898@gmail.com>
2026-03-30 20:29:14 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-let Auto_Pilot = 0
 controller.combos.attachCombo("Up Up Down Down Left Left Right Down", function () {
     music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
     Auto_Pilot = 1
 })
+let Auto_Pilot = 0
+controller.combos.setTimeout(3000)
 forever(function () {
     if (Auto_Pilot == 1) {
         let statusbar: StatusBarSprite = null
